92c9618797ab4fd87f56c4ced752113b5704255a
Chalkyy25 <Ashleychalk@sky.com>
2025-06-21 11:54:01 +0000

    Added routes for ServerShow

---
+++ app/Livewire/Pages/Admin/ServerShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Admin;
+
+use Livewire\Component;
+
+class ServerShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.admin.server-show');
+    }
+}
