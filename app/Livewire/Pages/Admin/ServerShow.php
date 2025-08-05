03a5389384eec9948eb5d671e539bd1ca34775c3
Chalkyy25 <Ashleychalk@sky.com>
2025-08-05 20:08:42 +0100

    Update ServerShow.php

--- app/Livewire/Pages/Admin/ServerShow.php
+++ app/Livewire/Pages/Admin/ServerShow.php
@@ -124,7 +124,7 @@ class ServerShow extends Component
         }
     }
 
-    public function deleteServer(): void
+    public function deleteServer():
     {
         $name = $this->vpnServer->name;
         $this->vpnServer->delete();
