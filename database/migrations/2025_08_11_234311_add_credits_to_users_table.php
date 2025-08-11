475ff5afc78141dcaa5590555ed6b9bd54340981
Chalkyy25 <Ashleychalk@sky.com>
2025-08-12 00:44:51 +0100

    Update 2025_08_11_234311_add_credits_to_users_table.php

--- database/migrations/2025_08_11_234311_add_credits_to_users_table.php
+++ database/migrations/2025_08_11_234311_add_credits_to_users_table.php
@@ -10,11 +10,11 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->integer('credits')->default(0);
+    });
+}
 
     /**
      * Reverse the migrations.
