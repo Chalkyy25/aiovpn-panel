cc5e739d06e537e8227b9a374cba6d07284c88d1
Chalkyy25 <Ashleychalk@sky.com>
2025-08-13 23:21:54 +0100

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,7 +1,22 @@
-@props(['class' => 'h-10 w-auto'])
+@props([
+    'type' => 'mark', // 'mark' | 'full'
+    'alt'  => 'AIO VPN',
+])
 
-<img
-    src="{{ asset('images/logo.svg') }}"
-    alt="AIO VPN"
-    {{ $attributes->merge(['class' => $class]) }}
-/>
+@if ($type === 'mark')
+    <img
+        src="{{ asset('images/branding/logo-mark.svg') }}"
+        alt="{{ $alt }}"
+        {{ $attributes->merge(['class' => 'h-9 w-9 rounded-md ring-1 ring-black/5 dark:ring-white/10']) }}
+        loading="eager"
+        decoding="async"
+    >
+@else
+    <img
+        src="{{ asset('images/branding/logo-full.svg') }}"
+        alt="{{ $alt }}"
+        {{ $attributes->merge(['class' => 'h-8 w-auto']) }}
+        loading="eager"
+        decoding="async"
+    >
+@endif
