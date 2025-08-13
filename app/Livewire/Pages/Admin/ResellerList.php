6456f69e0a33ff8e2d644e5575267126493d6631
Chalkyy25 <Ashleychalk@sky.com>
2025-08-13 01:45:34 +0100

    Update ResellerList.php

--- app/Livewire/Pages/Admin/ResellerList.php
+++ app/Livewire/Pages/Admin/ResellerList.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Pages\Admin;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ResellerList extends Component
 {
+    use WithPagination;
+
+    public string $search = '';
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.pages.admin.reseller-list');
+        $resellers = User::query()
+            ->where('role', 'reseller')
+            ->when($this->search, function ($query) {
+                $query->where('name', 'like', "%{$this->search}%")
+                      ->orWhere('email', 'like', "%{$this->search}%");
+            })
+            ->latest()
+            ->paginate(10);
+
+        return view('livewire.pages.admin.reseller-list', [
+            'resellers' => $resellers
+        ]);
     }
 }
