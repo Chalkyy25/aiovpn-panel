20e8e661cc88f5fc9f2325962e7b7355422e9fe8
Chalkyy25 <Ashleychalk@sky.com>
2025-08-13 23:29:05 +0100

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,22 +1,29 @@
 @props([
-    'type' => 'mark', // 'mark' | 'full'
+    // 'mark' = small square for navbar, 'full' = wider version for auth pages
+    'type' => 'mark',
     'alt'  => 'AIO VPN',
 ])
 
+@php
+    // Using the same file for both variants. If you later add two files,
+    // change these to branding/logo-mark.svg and branding/logo-full.svg.
+    $src = asset('images/logo.svg');
+
+    $common = $attributes->merge([
+        'alt'      => $alt,
+        'loading'  => 'eager',
+        'decoding' => 'async',
+    ]);
+@endphp
+
 @if ($type === 'mark')
     <img
-        src="{{ asset('images/logo.svg') }}"
-        alt="{{ $alt }}"
-        {{ $attributes->merge(['class' => 'h-9 w-9 rounded-md ring-1 ring-black/5 dark:ring-white/10']) }}
-        loading="eager"
-        decoding="async"
+        src="{{ $src }}"
+        {{ $common->merge(['class' => 'h-9 w-9 rounded-md ring-1 ring-black/5 dark:ring-white/10 object-contain bg-white']) }}
     >
 @else
     <img
-        src="{{ asset('images/logo.svg') }}"
-        alt="{{ $alt }}"
-        {{ $attributes->merge(['class' => 'h-8 w-auto']) }}
-        loading="eager"
-        decoding="async"
+        src="{{ $src }}"
+        {{ $common->merge(['class' => 'h-12 sm:h-14 w-auto object-contain']) }}
     >
 @endif
