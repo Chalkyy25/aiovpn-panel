126e1c6ef909f2a98b842043f76c24130f387c18
Chalkyy25 <Ashleychalk@sky.com>
2025-08-14 23:11:10 +0100

    Update NavLink.php

--- app/View/Components/NavLink.php
+++ app/View/Components/NavLink.php
@@ -2,24 +2,21 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class NavLink extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public bool $active;
+    public ?string $icon;
+
+    public function __construct($active = false, ?string $icon = null)
     {
-        //
+        // Normalize truthy values from Blade (bool or string)
+        $this->active = filter_var($active, FILTER_VALIDATE_BOOLEAN);
+        $this->icon   = $icon;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.nav-link');
     }
