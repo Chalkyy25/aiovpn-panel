b6f30d802cb3bafe8313c6add5ebf783bad79fb1
Chalkyy25 <Ashleychalk@sky.com>
2025-08-15 00:44:28 +0100

    Update section-card.blade.php

--- resources/views/components/section-card.blade.php
+++ resources/views/components/section-card.blade.php
@@ -1,6 +1,5 @@
-@props(['title' => ''])
 <div class="bg-white border rounded-xl shadow-sm">
-    @if($title)
+    @if(!empty($title))
         <div class="px-4 py-3 border-b">
             <h2 class="font-semibold">{{ $title }}</h2>
         </div>
