b35896e98949adffb1cab5c90569175676f914d4
Chalkyy25 <Ashleychalk@sky.com>
2025-08-14 23:23:28 +0100

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,35 +1,40 @@
-{{-- Vertical sidebar (collapsible on desktop, drawer on mobile via app.blade.php) --}}
 <nav class="px-2 pb-4 space-y-1 text-sm" aria-label="Sidebar">
-
     {{-- MAIN --}}
-    <x-nav-link href="{{ route('admin.dashboard') }}"
-                :active="request()->routeIs('admin.dashboard')"
-                icon="o-home">
+    <x-side-link href="{{ route('admin.dashboard') }}"
+                 :active="request()->routeIs('admin.dashboard')"
+                 icon="o-home">
         Dashboard
-    </x-nav-link>
+    </x-side-link>
 
-    <x-nav-link href="{{ route('admin.vpn-dashboard') }}"
-                :active="request()->routeIs('admin.vpn-dashboard')"
-                icon="o-chart-bar">
+    <x-side-link href="{{ route('admin.vpn-dashboard') }}"
+                 :active="request()->routeIs('admin.vpn-dashboard')"
+                 icon="o-chart-bar">
         VPN Monitor
-    </x-nav-link>
+    </x-side-link>
+
+    {{-- MOVED: Servers just under VPN Monitor --}}
+    <x-side-link href="{{ route('admin.servers.index') }}"
+                 :active="request()->routeIs('admin.servers.*')"
+                 icon="o-server">
+        Servers
+    </x-side-link>
 
     {{-- USERS --}}
     <div class="mt-3">
         <div class="px-3 text-[11px] uppercase tracking-wide text-gray-500"
              x-show="!$root.sidebarCollapsed">Users</div>
 
-        <x-nav-link href="{{ route('admin.resellers.create') }}"
-                    :active="request()->routeIs('admin.resellers.create')"
-                    icon="o-plus">
+        <x-side-link href="{{ route('admin.resellers.create') }}"
+                     :active="request()->routeIs('admin.resellers.create')"
+                     icon="o-plus">
             Add User
-        </x-nav-link>
+        </x-side-link>
 
-        <x-nav-link href="{{ route('admin.resellers.index') }}"
-                    :active="request()->routeIs('admin.resellers.index')"
-                    icon="o-user-group">
+        <x-side-link href="{{ route('admin.resellers.index') }}"
+                     :active="request()->routeIs('admin.resellers.index')"
+                     icon="o-user-group">
             Manage Users
-        </x-nav-link>
+        </x-side-link>
     </div>
 
     {{-- LINES --}}
@@ -37,40 +42,36 @@
         <div class="px-3 text-[11px] uppercase tracking-wide text-gray-500"
              x-show="!$root.sidebarCollapsed">Lines</div>
 
-        <x-nav-link href="{{ route('admin.vpn-users.create') }}"
-                    :active="request()->routeIs('admin.vpn-users.create')"
-                    icon="o-plus-circle">
+        <x-side-link href="{{ route('admin.vpn-users.create') }}"
+                     :active="request()->routeIs('admin.vpn-users.create')"
+                     icon="o-plus-circle">
             Add Line
-        </x-nav-link>
+        </x-side-link>
 
-        <x-nav-link href="{{ route('admin.vpn-users.trial') }}"
-                    :active="request()->routeIs('admin.vpn-users.trial')"
-                    icon="o-clock">
+        <x-side-link href="{{ route('admin.vpn-users.trial') }}"
+                     :active="request()->routeIs('admin.vpn-users.trial')"
+                     icon="o-clock">
             Generate Trial
-        </x-nav-link>
+        </x-side-link>
 
-        <x-nav-link href="{{ route('admin.vpn-users.index') }}"
-                    :active="request()->routeIs('admin.vpn-users.index')"
-                    icon="o-list-bullet">
+        <x-side-link href="{{ route('admin.vpn-users.index') }}"
+                     :active="request()->routeIs('admin.vpn-users.index')"
+                     icon="o-list-bullet">
             Manage Lines
-        </x-nav-link>
+        </x-side-link>
     </div>
 
-    {{-- SERVERS --}}
-    <x-nav-link href="{{ route('admin.servers.index') }}"
-                :active="request()->routeIs('admin.servers.*')"
-                icon="o-server">
-        Servers
-    </x-nav-link>
-
     {{-- SETTINGS --}}
-    <x-nav-link href="{{ route('admin.settings') }}"
-                :active="request()->routeIs('admin.settings')"
-                icon="o-cog-6-tooth">
+    <x-side-link href="{{ route('admin.settings') }}"
+                 :active="request()->routeIs('admin.settings')"
+                 icon="o-cog-6-tooth">
         Settings
-    </x-nav-link>
+    </x-side-link>
 
-    {{-- CREDITS (admins/resellers) --}}
+    {{-- DIVIDER (the blue line area in your screenshot) --}}
+    <div class="my-4 h-px bg-gray-200"></div>
+
+    {{-- CREDITS --}}
     @auth
         @php
             $u = auth()->user();
@@ -79,9 +80,9 @@
             $creditsUrl = $isAdmin ? route('admin.credits') : ($isReseller ? route('reseller.credits') : '#');
         @endphp
         @if ($isAdmin || $isReseller)
-            <x-nav-link href="{{ $creditsUrl }}" icon="o-banknotes">
+            <x-side-link href="{{ $creditsUrl }}" icon="o-banknotes">
                 Credits: {{ $u->credits }}
-            </x-nav-link>
+            </x-side-link>
         @endif
     @endauth
 </nav>
