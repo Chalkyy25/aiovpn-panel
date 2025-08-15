4ad9cef4c7d17a1f59612fc6104e25e1c03c5c71
Chalkyy25 <Ashleychalk@sky.com>
2025-08-15 01:25:51 +0100

    Update section-card.blade.php

--- resources/views/components/section-card.blade.php
+++ resources/views/components/section-card.blade.php
@@ -1,10 +1,12 @@
-<div class="bg-white border rounded-xl shadow-sm">
-    @if(!empty($title))
-        <div class="px-4 py-3 border-b">
-            <h2 class="font-semibold">{{ $title }}</h2>
-        </div>
-    @endif
-    <div class="p-4">
-        {{ $slot }}
+@props(['title' => null])
+
+<div {{ $attributes->merge(['class' => 'aio-card']) }}>
+  @if($title)
+    <div class="px-4 py-3 border-b aio-divider">
+      <h2 class="font-semibold">{{ $title }}</h2>
     </div>
+  @endif
+  <div class="p-4">
+    {{ $slot }}
+  </div>
 </div>
