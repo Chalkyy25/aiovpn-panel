ca083d482766e28e3296ee4151e6c80ae0cd3cd1
Chalkyy25 <Ashleychalk@sky.com>
2025-08-18 22:55:49 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\DB;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory;
+    use HasApiTokens, Notifiable;
 
     /**
      * Mass-assignable columns.
