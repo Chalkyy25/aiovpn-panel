2b813475c65a2317cb220dc6a79fed6b11ebac5a
Chalkyy25 <Ashleychalk@sky.com>
2025-08-19 18:28:00 +0100

    Update ServerMgmtEvent.php

--- app/Events/ServerMgmtEvent.php
+++ app/Events/ServerMgmtEvent.php
@@ -2,7 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
