6da178b0f0aa4aaad20bb16fe8053db409bdeba6
Chalkyy25 <Ashleychalk@sky.com>
2025-08-20 21:50:34 +0100

    Update reverb.php

--- config/reverb.php
+++ config/reverb.php
@@ -11,6 +11,23 @@ return [
             'options' => [
                 'tls' => [],
             ],
+            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10000),
+
+            // 👇 REQUIRED, even if you don’t use scaling
+            'scaling' => [
+                'enabled' => env('REVERB_SCALING_ENABLED', false),
+                'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
+                'server' => [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'port' => env('REDIS_PORT', 6379),
+                    'password' => env('REDIS_PASSWORD'),
+                    'database' => env('REDIS_DB', 0),
+                    'timeout' => env('REDIS_TIMEOUT', 60),
+                ],
+            ],
+
+            'pulse_ingest_interval' => 15,
+            'telescope_ingest_interval' => 15,
         ],
     ],
 
@@ -25,6 +42,10 @@ return [
                 'scheme' => env('REVERB_SCHEME', 'https'),
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
             ],
+            'allowed_origins' => ['*'],
+            'ping_interval' => 60,
+            'activity_timeout' => 30,
+            'max_message_size' => 10000,
         ],
     ],
 ];
