8a4bd6713778b4780b70dca2a51570388ddb2231
Chalkyy25 <Ashleychalk@sky.com>
2025-08-20 13:56:17 +0100

    Update reverb.php

--- config/reverb.php
+++ config/reverb.php
@@ -1,94 +1,50 @@
-<?php
-
 return [
+  'default' => env('REVERB_SERVER', 'reverb'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Reverb Server
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default server used by Reverb to handle
-    | incoming messages as well as broadcasting message to all your
-    | connected clients. At this time only "reverb" is supported.
-    |
-    */
-
-    'default' => env('REVERB_SERVER', 'reverb'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Reverb Servers
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define details for each of the supported Reverb servers.
-    | Each server has its own configuration options that are defined in
-    | the array below. You should ensure all the options are present.
-    |
-    */
-
-    'servers' => [
-
-        'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
-            'path' => env('REVERB_SERVER_PATH', ''),
-            'hostname' => env('REVERB_HOST'),
-            'options' => [
-                'tls' => [],
-            ],
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
-            'scaling' => [
-                'enabled' => env('REVERB_SCALING_ENABLED', false),
-                'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
-                'server' => [
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'database' => env('REDIS_DB', '0'),
-                    'timeout' => env('REDIS_TIMEOUT', 60),
-                ],
-            ],
-            'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
-            'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
+  'servers' => [
+    'reverb' => [
+      // Bind the Reverb server only on localhost; Nginx terminates TLS.
+      'host' => env('REVERB_SERVER_HOST', '127.0.0.1'),
+      'port' => env('REVERB_SERVER_PORT', 8080),
+      'path' => env('REVERB_SERVER_PATH', ''),
+      'hostname' => env('REVERB_HOST', 'reverb.aiovpn.co.uk'),
+      'options' => [
+        'tls' => [], // empty because Nginx does TLS
+      ],
+      'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10000),
+      'scaling' => [
+        'enabled' => env('REVERB_SCALING_ENABLED', false),
+        'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
+        'server' => [
+          'host' => env('REDIS_HOST', '127.0.0.1'),
+          'port' => env('REDIS_PORT', 6379),
+          'password' => env('REDIS_PASSWORD'),
+          'database' => env('REDIS_DB', 0),
+          'timeout' => env('REDIS_TIMEOUT', 60),
         ],
-
+      ],
+      'pulse_ingest_interval' => 15,
+      'telescope_ingest_interval' => 15,
     ],
+  ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Reverb Applications
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define how Reverb applications are managed. If you choose
-    | to use the "config" provider, you may define an array of apps which
-    | your server will support, including their connection credentials.
-    |
-    */
-
-    'apps' => [
-
-        'provider' => 'config',
-
-        'apps' => [
-            [
-                'key' => env('REVERB_APP_KEY'),
-                'secret' => env('REVERB_APP_SECRET'),
-                'app_id' => env('REVERB_APP_ID'),
-                'options' => [
-                    'host' => env('REVERB_HOST'),
-                    'port' => env('REVERB_PORT', 443),
-                    'scheme' => env('REVERB_SCHEME', 'https'),
-                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
-                ],
-                'allowed_origins' => ['*'],
-                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
-                'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
-                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
-            ],
-        ],
-
-    ],
-
+  'apps' => [
+    'provider' => 'config',
+    'apps' => [[
+      'key' => env('REVERB_APP_KEY', 'localkey'),
+      'secret' => env('REVERB_APP_SECRET', 'localsecret'),
+      'app_id' => env('REVERB_APP_ID', '1'),
+      'options' => [
+        'host' => env('REVERB_HOST', 'reverb.aiovpn.co.uk'),
+        'port' => env('REVERB_PORT', 443),
+        'scheme' => env('REVERB_SCHEME', 'https'),
+        'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+      ],
+      // lock this down if you want: 'https://panel.aiovpn.co.uk'
+      'allowed_origins' => ['*'],
+      'ping_interval' => 60,
+      'activity_timeout' => 30,
+      'max_message_size' => 10000,
+    ]],
+  ],
 ];
