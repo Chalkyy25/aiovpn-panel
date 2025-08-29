0725f662d0412b3598d3a0241082055e730befe3
Chalkyy25 <Ashleychalk@sky.com>
2025-08-29 02:33:19 +0100

    Update input.blade.php

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,8 +2,15 @@
 
 <div>
     @if($label)
-        <label class="block text-sm font-medium text-gray-700 mb-1">{{ $label }}</label>
+        <label for="{{ $name }}" class="block text-sm font-medium text-gray-700 mb-1">
+            {{ $label }}
+        </label>
     @endif
-<input {{ $attributes->merge(['class' => 'border rounded w-full px-3 py-2']) }} />
 
+    <input
+        name="{{ $name }}"
+        id="{{ $name }}"
+        type="{{ $type }}"
+        {{ $attributes->merge(['class' => 'border rounded w-full px-3 py-2']) }}
+    />
 </div>
