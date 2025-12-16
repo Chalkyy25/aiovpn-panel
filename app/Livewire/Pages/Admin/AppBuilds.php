14d983c129c91a49c95e303c452e47cda8513f99
Chalkyy25 <Ashleychalk@sky.com>
2025-12-16 14:11:04 +0000

    Update AppBuilds.php

--- app/Livewire/Pages/Admin/AppBuilds.php
+++ app/Livewire/Pages/Admin/AppBuilds.php
@@ -29,8 +29,7 @@ class AppBuilds extends Component
             'version_name'  => 'required|string|max:50',
             'mandatory'     => 'boolean',
             'release_notes' => 'nullable|string|max:2000',
-            'apk' => 'required|file|mimes:apk|max:200000',        ];
-    }
+            'apk' => 'required|file|mimetypes:application/vnd.android.package-archive,application/octet-stream,application/zip|max:200000',    }
 
     public function ping(): void
     {
