d6b407110f6a04cf15113fca0fe7f356f7a5270a
Chalkyy25 <Ashleychalk@sky.com>
2025-12-21 12:58:50 +0000

    Update create-vpn-user.blade.php

--- resources/views/livewire/pages/admin/create-vpn-user.blade.php
+++ resources/views/livewire/pages/admin/create-vpn-user.blade.php
@@ -1,6 +1,6 @@
 @php $isAdmin = auth()->user()?->role === 'admin'; @endphp
 
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 space-y-6">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 space-y-6 [color-scheme:dark]">
 
     <div class="flex items-center justify-between">
         <div>
