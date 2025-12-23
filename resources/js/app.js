f3b9503bd3d757204f86755ee919f8db6bf1560f
Chalkyy25 <Ashleychalk@sky.com>
2025-12-23 11:31:37 +0000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import './bootstrap';           // this sets up Echo/Reverb
 import.meta.glob(['../images/**', '../fonts/**']);
 import 'flowbite'
-import './dashboard/vpn-dashboard';
+import './dashboard/vpnDashboard.js';
