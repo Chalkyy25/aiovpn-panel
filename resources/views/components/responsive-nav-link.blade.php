18e41305e02a7268d9d0383b9de36c88f51aed8d
Chalkyy25 <Ashleychalk@sky.com>
2026-02-06 16:32:30 +0000

    Update responsive-nav-link.blade.php

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -7,14 +7,32 @@
 ])
 
 @php
-    // Forward props into nav-link, ensuring collapseAware is false for mobile
-    $classes = $attributes->class('block'); 
+    // Normalize icon names (same rules as nav-link)
+    // - o-home        -> heroicon-o-home
+    // - s-user        -> heroicon-s-user
+    // - heroicon-*    -> unchanged
+    $iconName = null;
+
+    if (!empty($icon)) {
+        $icon = (string) $icon;
+
+        if (str_starts_with($icon, 'heroicon-')) {
+            $iconName = $icon;
+        } elseif (str_starts_with($icon, 'o-') || str_starts_with($icon, 's-')) {
+            $iconName = 'heroicon-' . $icon;
+        } else {
+            $iconName = $icon;
+        }
+    }
+
+    // Force mobile behaviour
+    $classes = $attributes->class('block');
 @endphp
 
 <x-nav-link
     :href="$href"
     :active="$active"
-    :icon="$icon"
+    :icon="$iconName"
     :variant="$variant"
     :collapse-aware="false"
     {{ $classes }}
