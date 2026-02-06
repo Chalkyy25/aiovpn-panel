0c8c85d17387f9948c3c288f1d66dc5fadda58ec
Chalkyy25 <Ashleychalk@sky.com>
2026-02-06 16:09:20 +0000

    Change admin panel path from 'admin' to 'filament'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,7 +24,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('filament')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
