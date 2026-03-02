84b54b29fde201ecd2ef32e9e97c5dcd05417b1e
Chalkyy25 <Ashleychalk@sky.com>
2026-03-02 19:51:41 +0000

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -16,7 +16,7 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
-    protected static ?string $navigationLabel = 'Users';
+    protected static ?string $navigationLabel = 'Staff';
     protected static ?string $navigationGroup = 'Admin';
     protected static ?int $navigationSort = 1;
 
