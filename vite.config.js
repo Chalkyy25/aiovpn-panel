cefb8c6cd39b75a2d8733e51b325d2fbed78f2dc
Chalkyy25 <Ashleychalk@sky.com>
2026-03-02 18:56:20 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/filament/admin/theme.css', // ✅ ADD THIS
+                'resources/css/filament/reseller/theme.css', // optional if using reseller theme
             ],
             refresh: true,
         }),
