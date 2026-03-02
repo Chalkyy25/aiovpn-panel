7c3776c791b5ada0f25ca865c99005243e2e7bc5
Chalkyy25 <Ashleychalk@sky.com>
2026-03-02 15:43:00 +0000

    Update PackageResource.php

--- app/Filament/Resources/PackageResource.php
+++ app/Filament/Resources/PackageResource.php
@@ -17,7 +17,9 @@ class PackageResource extends Resource
 {
     protected static ?string $model = Package::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Billing';
+protected static ?string $navigationIcon  = 'heroicon-o-rectangle-stack';
+protected static ?int $navigationSort     = 1;
 
     public static function form(Form $form): Form
     {
