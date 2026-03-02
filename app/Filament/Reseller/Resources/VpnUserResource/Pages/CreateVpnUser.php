063a72a0927bd072549138b4d62acf16ca7feb04
Chalkyy25 <Ashleychalk@sky.com>
2026-03-02 22:59:02 +0000

    Update CreateVpnUser.php

--- app/Filament/Reseller/Resources/VpnUserResource/Pages/CreateVpnUser.php
+++ app/Filament/Reseller/Resources/VpnUserResource/Pages/CreateVpnUser.php
@@ -3,10 +3,21 @@
 namespace App\Filament\Reseller\Resources\VpnUserResource\Pages;
 
 use App\Filament\Reseller\Resources\VpnUserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateVpnUser extends CreateRecord
 {
     protected static string $resource = VpnUserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // lock ownership
+        $data['client_id'] = auth()->id();
+
+        // defaults if missing
+        $data['is_active'] = $data['is_active'] ?? true;
+        $data['max_connections'] = $data['max_connections'] ?? 1;
+
+        return $data;
+    }
 }
