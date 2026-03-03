fe6c87109e826a1d4d955d82dd58f9f9cd14e65c
Chalkyy25 <Ashleychalk@sky.com>
2026-03-03 23:24:10 +0000

    changes

---
+++ database/migrations/2026_03_03_231243_add_unique_wireguard_address_to_vpn_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::table('vpn_users', function (Blueprint $table) {
+        $table->string('wireguard_address')->nullable()->unique()->change();
+    });
+}
+
+public function down(): void
+{
+    Schema::table('vpn_users', function (Blueprint $table) {
+        $table->dropUnique(['wireguard_address']);
+        $table->string('wireguard_address')->nullable()->change();
+    });
+}
+
+};
