7bccb8127cd0b86e3b9d52abe5c44ebb29424a65
Chalkyy25 <Ashleychalk@sky.com>
2026-03-04 00:13:59 +0000

    Create client.blade.php

---
+++ resources/views/layouts/client.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('layouts.partials.head')
+</head>
+<body class="min-h-screen">
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
