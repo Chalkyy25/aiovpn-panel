95c6ee2a886008fe0934765a1bbb413465b2f65a
Chalkyy25 <Ashleychalk@sky.com>
2026-05-10 14:15:04 +0000

    migration

---
+++ app/Console/Commands/PollServerHealth.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PollServerHealth extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:poll-server-health';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
