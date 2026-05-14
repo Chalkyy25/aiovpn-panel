c81a161a8407124a7d5c54ce28af067a6c1492a9
Chalkyy25 <Ashleychalk@sky.com>
2026-05-14 13:39:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+
+        hmr: {
+            protocol: 'wss',
+            host: process.env.CODESPACE_NAME
+                ? `${process.env.CODESPACE_NAME}-5173.app.github.dev`
+                : 'localhost',
+        },
+    },
+
     plugins: [
         laravel({
             input: [
